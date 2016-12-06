7aaca9042220dfefa88d2712c6c4efd01b033773
Zach <zachary.ladlie@gmail.com>
2016-12-06 01:15:17 -0600

    Removed use of cached topology size since actually, each tree topology depends on the number of child nodes and type of child nodes (e.g. tile node)

--- FirstPerson/Plugins/VoxelMate/Source/VoxelMate/Private/ArchiveGrid.cpp
+++ FirstPerson/Plugins/VoxelMate/Source/VoxelMate/Private/ArchiveGrid.cpp
@@ -10,7 +10,6 @@
 
 bool FVoxelDatabaseTypeFactory<openvdb::GridBase>::IsInitialized = false;
 TArray<FString> FVoxelDatabaseTypeFactory<openvdb::GridBase>::RegisteredTypeNames;
-TMap<FString, int32> FGridFactory::CachedTopologySizeByTreeType;
 
 bool FVoxelDatabaseTypeFactory<openvdb::GridBase>::IsRegisteredType(const FString& TypeName)
 {
@@ -119,20 +118,16 @@ FArchive& operator<<(FArchive& Ar, openvdb::GridBase& Grid)
         Grid.setSaveFloatAsHalf(FloatSavedAsHalf);
     }
 
-    const openvdb::TreeBase& Tree = Grid.baseTree();
-    const openvdb::Name& TreeType = Grid.baseTree().type();
+    int64 TopologyByteCount = 0;
+    TArray<ANSICHAR> TopologyDataBytes;
 
-    //Save the topology byte count for each tree type so that we don't have to keep calculating it over and over for many trees
-    int32& TopologyByteCount = FGridFactory::CachedTopologySizeByTreeType.FindOrAdd(UTF8_TO_TCHAR(TreeType.c_str()));
-
-    //Peek at the byte count of the array (if saving and the size is currently unknown, write an initial 0)
-    const int64 TopologyByteCountPos = Ar.Tell();
-    Ar << TopologyByteCount;
-
-    if (TopologyByteCount == 0)
+    if (Ar.IsLoading())
     {
-        check(!IsLoading); //If loading but the topology byte count was 0 then something went wrong while saving
-
+        TopologyDataBytes.BulkSerialize(Ar);
+        TopologyByteCount = (int64)TopologyDataBytes.Num();
+    }
+    else
+    {
         //Calculate the unknown topology size by writing to a back_inserter
         std::vector<ANSICHAR> TopologyBytes;
         boost::iostreams::back_insert_device<std::vector<ANSICHAR>> TopologySink(TopologyBytes);
@@ -146,20 +141,8 @@ FArchive& operator<<(FArchive& Ar, openvdb::GridBase& Grid)
         TempTopologyStream.open(TopologySink);
         Grid.writeTopology(static_cast<OutputStreamType&>(TempTopologyStream));
         TempTopologyStream.close();
-
-        const size_t ByteCount = TopologyBytes.size();
-        TopologyByteCount = ByteCount;
-    }
-
-    TArray<ANSICHAR> TopologyDataBytes;
-    if (IsLoading)
-    {
-        TopologyDataBytes.BulkSerialize(Ar);
-        check(TopologyByteCount == TopologyDataBytes.Num());
-    }
-    else
-    {
-        TopologyDataBytes.SetNumUninitialized(TopologyByteCount);
+        TopologyByteCount = (int64)TopologyBytes.size();
+        TopologyDataBytes.SetNumUninitialized((int32)TopologyByteCount);
     }
 
     ANSICHAR* TopologyBufferStart = TopologyDataBytes.GetData();
